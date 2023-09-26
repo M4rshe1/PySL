20e4ab7f18df8e6ed6b2df5f71376e32d52407ba
COlin Heggli <colin.heggli@gmail.com>
2023-09-26 07:00:47 +0200

    make it better

--- interview/automorphic_number.py
+++ interview/automorphic_number.py
@@ -1,2 +1,4 @@
+# This file contains the solution for the Automorphism Number problem.
+# A number is called Automorphism number if and only if its square ends in the same digits as the number itself.
 def check_number(number):
     return str(number ** 2).endswith(str(number))
